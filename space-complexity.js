1d79af7f32d25f77cad4747d94a546a82e11d9a8
hasanmiadev <hasanmiadev@gmail.com>
2023-05-23 10:58:37 +0600

    space-complexity

---
+++ space-complexity.js
@@ -0,0 +1,20 @@
+const sum = (arr) => {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total = total + arr[i]
+    }
+    return total
+}
+
+console.log(sum([23,4,6])); // O(1)
+
+
+const double = (arr) => {
+    let newArr = [];
+    for(let i = 0; i< arr.length; i++){
+        newArr.push(arr[i] * 2);
+    }
+    return newArr
+}
+
+console.log(double([3,4,5])); // O(n)
